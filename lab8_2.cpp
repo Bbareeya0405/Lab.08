b74ff6d8f0441279395e3ab0db4e3db2f1f6a2f6
BBAER <yeybreya@gmail.com>
2024-12-06 18:08:45 +0700

    lab8_2.cpp

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -28,11 +28,9 @@ int main(){
 	
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
-		cin.ignore(); // To ignore the remaining newline character
-		// [Missing Code 2] Get name of the i-th students that may include whitespace.
-		getline(cin, name[i]);  // Use getline to allow spaces in the name
+		cin.ignore();
+		getline(cin, name[i]);
 		cout << "Score of student " << i+1 << ": ";
-		// [Missing Code 3] Get score of the i-th students.
 		cin >> score[i];
 		i++;
 	}
